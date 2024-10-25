7a5c650a2d4a724c59d05c4008ba3c01e2d8521d
Giovani <tassogiovani@gmail.com>
2024-10-25 19:19:58 -0300

    começa das telas dos clientes

---
+++ src/main/frontend/generated/theme-bigodeira.components.generated.js
@@ -0,0 +1,14 @@
+
+
+
+if (!document['_vaadintheme_bigodeira_componentCss']) {
+  
+  document['_vaadintheme_bigodeira_componentCss'] = true;
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept((module) => {
+    window.location.reload();
+  });
+}
+
